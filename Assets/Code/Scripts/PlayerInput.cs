ee663347e3a6f67d2cee0507939a2c1fcc08e6cf
Samuel Einheri <Samuel.einheri@gmail.com>
2020-09-29 12:18:30 +0200

    Moving player along Y-axis works as intended, we're just dumb

--- Assets/Code/Scripts/PlayerInput.cs
+++ Assets/Code/Scripts/PlayerInput.cs
@@ -14,7 +14,7 @@ namespace GT
 
         PlayerControls controls;
 
-        [Tooltip("Maximum speed that a Player can achieve"), Range(1f, 30f)]
+        [Tooltip("Maximum speed that a Player can achieve"), Range(1f, 500f)]
         public float maxSpeed = 15f;
         public float rotationSpeed = 15f;
         //public float steeringPower = 15f;
@@ -59,10 +59,12 @@ namespace GT
 
         void PlayerMove()
         {
-
             Vector2 movePlayer = new Vector2(_move.x, _move.y) * Time.deltaTime;
-            transform.Translate(movePlayer * maxSpeed, Space.World);
+            //transform.Translate(movePlayer * maxSpeed, Space.World);
+            Debug.Log("Moving: " + _move.y);
 
+            _playerBody.AddForce(movePlayer * maxSpeed);
+            Debug.Log(movePlayer * maxSpeed);
         }
 
         void PlayerRotate()
@@ -71,7 +73,7 @@ namespace GT
             Vector3 rotatePlayer = new Vector3(0, 0, -_rotate.x) * Time.deltaTime;
             //var direction = Mathf.Sign(Vector2.Dot(_playerBody.velocity, _playerBody.GetRelativeVector(Vector2.up)));
             //_playerBody.rotation += steeringAmount * steeringPower * _playerBody.velocity.magnitude * direction;
-            Debug.Log("rotating: " + _rotate);
+            //Debug.Log("rotating: " + _rotate);
             //_playerBody.AddRelativeForce(rotatePlayer * _playerBody.velocity.magnitude * steeringAmount / 2);
             transform.Rotate(rotatePlayer * rotationSpeed, Space.World);
             //_playerBody.rotation (-rotatePlayer * _playerBody.magnitude * steerinAmount / 2)
